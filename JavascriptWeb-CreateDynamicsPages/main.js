b7712f4cfbcd06279416c78d0f0c84cf89a30ba1
Christian <christian.gross@philips.com>
2023-04-03 11:24:30 -0300

    using lists

--- JavascriptWeb-CreateDynamicsPages/main.js
+++ JavascriptWeb-CreateDynamicsPages/main.js
@@ -1,5 +1,49 @@
-function tocaSomPom(){
+function playSound(idAudioElement) {
+    document.querySelector(idAudioElement).play();
+}
+
+const keyList = document.querySelectorAll('.tecla');
+let i = 0;
+
+for (let i = 0; i < keyList.length; i++ ){
+    const key = keyList[i];
+    const instrument = key.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrument}`;
+
+    console.log(idAudio);
+
+    keyList[i].onclick = function () {
+        playSound(idAudio);
+    } 
+}
+
+/*
+function playSoundPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = playSoundPom;
+*/
+
+/*
+while (i < keyList.length){
+    const key = keyList[i];
+    const instrument = key.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrument}`;
+
+    console.log(idAudio);
+
+    keyList[i].onclick = function () {
+        playSound(idAudio);
+    } 
+    
+    i++;
+
+    console.log(i);
+}
+*/
+
